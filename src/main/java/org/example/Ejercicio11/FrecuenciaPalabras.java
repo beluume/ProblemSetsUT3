b83ad421164710aca455ab7bd1491e65693ea943
camiladacol <camila.dacol@correo.ucu.edu.uy>
2026-05-15 23:01:41 -0300

    Casi terminado el ejercicio 11

--- src/main/java/org/example/Ejercicio11/FrecuenciaPalabras.java
+++ src/main/java/org/example/Ejercicio11/FrecuenciaPalabras.java
@@ -1,4 +1,38 @@
 package org.example.Ejercicio11;
 
-public class FrecuenciaPalabras {
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FrecuenciaPalabras
+{
+    public static void main(String[] args)
+    {
+        HashMap<String, Integer> mapa = new HashMap<>();
+
+        try {
+            Scanner leer = new Scanner(new File("libro.txt"));
+
+            while (leer.hasNext()) {
+                String palabra = leer.next();
+                palabra = palabra.toLowerCase();
+                palabra = palabra.replace(".", "");
+                palabra = palabra.replace(",", "");
+                if (mapa.containsKey(palabra)) {
+                    int cantidad = mapa.get(palabra);
+                    mapa.put(palabra, cantidad + 1);
+                }  else {
+                    mapa.put(palabra, 1);
+                    }
+                }
+
+            leer.close();
+            System.out.println("Frecuencia palabras: ");
+            for (String palabra : mapa.keySet()) {
+                System.out.println(palabra + " - " + mapa.get(palabra));
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo");
+        }
+    }
 }
--- src/main/java/org/example/Ejercicio11/JustificacionEjercicio11.md
+++ src/main/java/org/example/Ejercicio11/JustificacionEjercicio11.md
@@ -7,3 +7,10 @@ Cada vez que se encuentra una palabra, se debe verificar si ya aparecio antes, s
 Como hay millones de palabras, el HashMap permite hacerlo de manera instantánea.
 Si lo hicieramos con una lista, cada vez que se encuentra la palabra habria que volver a recorrer toda la lista para verificar si ya está.
 
+Resolución del ejercicio:
+Se lee el archivo libro.txt palabr apor palabra con el scanner.
+Cada palabra se pasa a minuscula y se eliminan puntos y comas. Despues se guarda en un HashMap.
+Si la palabra exisye, se obtiene la cantidad actual y se suma 1, si no existe se agrega con valor 1.
+
+Analisis orden:
+El programa recorre cada palabra una vez sola, por lo que si sería O(x), siendo x la cantidad de palabras del archivo
